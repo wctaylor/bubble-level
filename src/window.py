b6fff0bda302c85b1dbd5d6ab604cfe946853ebc
Will Taylor <wctaylor@librem.one>
2021-04-20 11:51:49 -0400

    Add initial bubble plotting

--- src/window.py
+++ src/window.py
@@ -18,11 +18,86 @@
 from gi.repository import Gtk
 
 
+import numpy as np
+from matplotlib.figure import Figure
+from matplotlib.patches import Circle, Ellipse
+from matplotlib.backends.backend_gtk3cairo import (FigureCanvasGTK3Cairo
+                                                   as FigureCanvas)
+
 @Gtk.Template(resource_path='/org/gnome/BubbleLevel/window.ui')
 class BubbleLevelWindow(Gtk.ApplicationWindow):
     __gtype_name__ = 'BubbleLevelWindow'
 
-    label = Gtk.Template.Child()
+    h_container       = Gtk.Template.Child()
+    v_container       = Gtk.Template.Child()
+    circle_container  = Gtk.Template.Child()
+    dpi = 282.45
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        self.plot_horizontal_bubble()
+        self.plot_vertical_bubble()
+        self.plot_circle_bubble()
+
+    def plot_horizontal_bubble(self):
+        fig = Figure(figsize=(360/self.dpi, 108/self.dpi), dpi=self.dpi,
+                     frameon=False)
+        ax = fig.add_subplot()
+        canvas = FigureCanvas(fig)
+        canvas.set_size_request(360, 108)
+
+
+        center = (0.5, 0.10)
+        bubble = Ellipse(xy=center, width=0.25, height=0.15, angle=0)
+        ax.set(xlim=(0, 1), ylim=(0, 0.2))
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.axvline(x=0.35, color='black', linewidth=0.4)
+        ax.axvline(x=0.65, color='black', linewidth=0.4)
+        ax.add_patch(bubble)
+
+        canvas.show()
+        self.h_container.add(canvas)
+
+    def plot_vertical_bubble(self):
+        fig = Figure(figsize=(108/self.dpi, 360/self.dpi), dpi=self.dpi,
+                     frameon=False)
+        ax = fig.add_subplot()
+        canvas = FigureCanvas(fig)
+        canvas.set_size_request(108, 360)
+
+
+        center = (0.1, 0.5)
+        bubble = Ellipse(xy=center, width=0.15, height=0.25, angle=0)
+        ax.set(xlim=(0, 0.2), ylim=(0, 1))
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.axhline(y=0.35, color='black', linewidth=0.4)
+        ax.axhline(y=0.65, color='black', linewidth=0.4)
+        ax.add_patch(bubble)
+
+        canvas.show()
+        self.v_container.add(canvas)
+
+    def plot_circle_bubble(self):
+        fig = Figure(figsize=(252/self.dpi, 252/self.dpi), dpi=self.dpi,
+                     frameon=False)
+        ax = fig.add_subplot()
+        canvas = FigureCanvas(fig)
+        canvas.set_size_request(252, 252)
+
+        center = (0.5, 0.5)
+        bubble = Circle(xy=center, radius=0.1)
+        guide  = Circle(xy=center, radius=0.15, fill=False, color='black')
+        ax.set(xlim=(0, 1), ylim=(0, 1))
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.axhline(y=0.5, color='black', linewidth=0.4)
+        ax.axvline(x=0.5, color='black', linewidth=0.4)
+        ax.add_patch(bubble)
+        ax.add_patch(guide)
+
+        canvas.show()
+        self.circle_container.add(canvas)
+
